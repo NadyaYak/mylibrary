9271ed22b3e9584eed81b47fb629d79244c5dc01
Nadya <nadezhdayagubova@gmail.com>
2023-04-24 14:59:46 -0400

    commit

--- routes/books.js
+++ routes/books.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const router = express.Router()
+const multer = require('multer')
+const path = require('multer')
 const Book = require('../models/book')
+const uploadPath = path.join('public', Book.coverImageBasePath)
 const Author = require('../models/author')
-
+const upload =multer({
+    dest: uploadPath,
+    fileFilter:
+})
 //All Books Route
 router.get('/', async (req,res) =>{
  res.send('All books')
