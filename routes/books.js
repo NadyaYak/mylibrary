e2b246c322db742392490048f3d1a50a14ef4142
Nadya <nadezhdayagubova@gmail.com>
2023-04-24 11:35:32 -0400

    commit

--- routes/books.js
+++ routes/books.js
@@ -4,17 +4,17 @@ const Book = require('../models/book')
 
 //All Books Route
 router.get('/', async (req,res) =>{
- 
+ res.send('All books')
 })
 
 //New Book Route
 router.get('/new',(req,res) =>{
-    
+    res.send('New books')    
 })
 
 //Create Book Route
 router.post('/',async (req,res) =>{
-
+    res.send('Create books')
 
 })
 module.exports = router
