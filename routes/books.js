7ce80bb893421c7178dcc4ae8c8294992605281c
Nadya <nadezhdayagubova@gmail.com>
2023-04-24 12:21:39 -0400

    cm

--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Book = require('../models/book')
+const Author = require('../models/author')
 
 //All Books Route
 router.get('/', async (req,res) =>{
@@ -8,13 +9,25 @@ router.get('/', async (req,res) =>{
 })
 
 //New Book Route
-router.get('/new',(req,res) =>{
-    res.send('New books')    
+router.get('/new', async (req,res) =>{
+try {
+    const authors = await Author.find({})
+    const book = new Book()
+    res.render('/books/new', {
+        authors: authors,
+        book: book
+    })
+} catch {
+    res.redirect('/books')
+}
+
+
+    
 })
 
 //Create Book Route
 router.post('/',async (req,res) =>{
-    res.send('Create books')
+
 
 })
 module.exports = router
