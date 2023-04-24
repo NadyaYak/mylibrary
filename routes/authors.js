e15f091e8746f67033a123b0b1dd77eb9525e18e
Nadya <nadezhdayagubova@gmail.com>
2023-04-24 10:29:14 -0400

    ms

--- routes/authors.js
+++ routes/authors.js
@@ -6,13 +6,14 @@ const Author = require('../models/author')
 router.get('/', async (req,res) =>{
     let searchOptions ={}
     if (req.query.name !=null && req.query.name !=='') {
-searchOptions.name = new RegExp(req.query.name, 'i')
+ searchOptions.name = new RegExp(req.query.name, 'i')
     }
     try{
-const authors = await Author.find({searchOptions})
-res.render('authors/index', { 
+      const authors = await Author.find(searchOptions)
+      res.render('authors/index', { 
     authors: authors,
-    searchOptions: req.query })
+    searchOptions: req.query 
+   })
 } catch {
 res.redirect('/')
 }
