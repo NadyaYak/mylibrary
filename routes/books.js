312191396e570cf136fb560a2ea6f20463ed59a4
Nadya <nadezhdayagubova@gmail.com>
2023-04-25 11:31:39 -0400

    commit

--- routes/books.js
+++ routes/books.js
@@ -20,7 +20,10 @@ let query = Book.find()
         query =query.regex('title',new RegExp(req.query.title, 'i'))
     }
     if (req.query.publishedBefore !=null && req.query.publishedBefore != ''){
-        query =query.('title',new RegExp(req.query.title, 'i'))
+        query =query.lte('publishDate',req.query.publishedBefore)
+    }
+    if (req.query.publishedAfter !=null && req.query.publishedAfter != ''){
+        query =query.gte('publishDate',req.query.publishedAfter)
     }
 try {
     const books =  await query.exec()
